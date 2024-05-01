48214c2b79a58337fa807f5008bd8fe37351c201
Tim Harder <radhermit@gmail.com>
2024-05-01 12:54:56 -0600

    lib: bump pkgcraft-c version compat

--- lib/_pkgcraft_c.pm
+++ lib/_pkgcraft_c.pm
@@ -9,8 +9,8 @@ use FFI::CheckLib qw(find_lib_or_die);
 use FFI::Platypus;
 
 # version requirements for pkgcraft C library
-my $MIN_VERSION = version->parse('0.0.14');
-my $MAX_VERSION = version->parse('0.0.14');
+my $MIN_VERSION = version->parse('0.0.15');
+my $MAX_VERSION = version->parse('0.0.15');
 
 my $ffi = FFI::Platypus->new(api => 2);
 $ffi->lib(find_lib_or_die(
